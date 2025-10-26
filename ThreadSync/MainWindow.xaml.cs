8787ea165bb5db4bcbe1f276c28def17dab29994
Райан Гослинг <martsinovichivan@gmail.com>
2025-10-26 13:20:10 +0300

    new algoritm for readers-writers problem, but with array data

--- ThreadSync/MainWindow.xaml.cs
+++ ThreadSync/MainWindow.xaml.cs
@@ -5,6 +5,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
+using ThreadSync;
+
 namespace ReadersWriters
 {
     public partial class MainWindow : Window
@@ -15,22 +17,40 @@ namespace ReadersWriters
 
         private bool running = false;
 
-        private Semaphore readerSemaphore;
-        private SemaphoreSlim mutex = new(1, 1);
         private object lockObj = new();
+
+        int readers = 0;
+        int writers = 0;
+        int arraySize = 0;
+        int?[] value;
+        TrackedMutex[] cellLocks;
+
+        int maxReaders = 0;
+
+        //
+        // TODO сделать общие данные как список/массив
+        // на каждом элементе Mutex. одновременно в ячейку может писать/читать 1 поток
+        //
+        // писатель записывает только в пустые ячейки
+        // читатель считывает ячейку и забирает оттуда значение (т.е. делает пустой)
+        //
+        // Например: массив из 5 элементов. 5 писателей может одновременно писать, 5 читателей одновременно читать
+        // 
+        //
+        //
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        int readers = 0;
-        int writers = 0;
-        int value = 0;
-        int maxReaders = 0;
-
         private void Start_Click(object sender, RoutedEventArgs e)
         {
+            if (running)
+            {
+                return;
+            }
+
             if (!int.TryParse(ReadersBox.Text, out int readers) || readers <= 0)
             {
                 MessageBox.Show("Введите кол-во читателей (>0)", "Читатели", MessageBoxButton.OK, MessageBoxImage.Warning);
@@ -46,17 +66,27 @@ namespace ReadersWriters
                 MessageBox.Show("Введите макс. кол-во одновременных читателей (>0)", "Одновременные читатели", MessageBoxButton.OK, MessageBoxImage.Warning);
                 return;
             }
+            if (!int.TryParse(ArraySizeBox.Text, out int arraySize) || arraySize <= 0)
+            {
+                MessageBox.Show("Введите размер массива (>0)");
+                return;
+            }
 
-            // Очистка
             ReadersPanel.Items.Clear();
             WritersPanel.Items.Clear();
             readerLabels.Clear();
             writerLabels.Clear();
             QueueBox.Text = "-";
-            SharedValueBox.Text = "0";
+            UpdateArrayPanel();
 
-            readerSemaphore = new Semaphore(maxReaders, maxReaders);
-            value = 0;
+            value = new int?[arraySize];
+
+            cellLocks = new TrackedMutex[arraySize];
+            for (int i = 0; i < cellLocks.Length; i++)
+            {
+                cellLocks[i] = new TrackedMutex();
+            }
+
             running = true;
 
             for (int i = 0; i < readers; i++)
@@ -101,61 +131,121 @@ namespace ReadersWriters
         private void ReaderLoop(int id)
         {
             var label = readerLabels[id];
+            int? freeCell;
+            int? threadValue;
+
             while (running)
             {
-                Dispatcher.Invoke(() => label.Content = "Читатель " + id + 1 + ": ждёт");
-                QueueAdd("R-" + id + 1);
+                Dispatcher.Invoke(() => label.Content = "Читатель " + (id + 1) + ": ждёт");
+                QueueAdd("R" + id + 1);
 
-                readerSemaphore.WaitOne();
-                QueueRemove("R-" + id + 1);
+                freeCell = ReaderFindFreeCell();
+                while (freeCell == null)
+                {
+                    Thread.Sleep(100);
+                    freeCell = ReaderFindFreeCell();
+                }
 
-                Dispatcher.Invoke(() => label.Content = "Читатель " + id + 1 + ": читает");
+                if (!cellLocks[(int)freeCell].TryAcquire())
+                {
+                    continue;
+                }
+
+                QueueRemove("R" + id + 1);
+
+                Dispatcher.Invoke(() => label.Content = "Читатель " + (id + 1) + ": читает");
 
                 lock (lockObj)
                 {
-                    int value = this.value;
-                    Thread.Sleep(rand.Next(2000, 5000));
+                    Thread.Sleep(rand.Next(2000, 3000));
+                    threadValue = value[(int)freeCell];
+                    value[(int)freeCell] = null;
                 }
 
-                readerSemaphore.Release();
-                Dispatcher.Invoke(() => label.Content = "Читатель " + id + 1 + ": завершил");
+                cellLocks[(int)freeCell].Release();
 
-                Thread.Sleep(rand.Next(2000, 5000));
+                Dispatcher.Invoke(() => label.Content = "Читатель " + (id + 1) + ": завершил, " + threadValue);
+                UpdateArrayPanel();
+                Thread.Sleep(rand.Next(3000, 5000));
             }
-            Dispatcher.Invoke(() => label.Content = "Читатель " + id + 1 + ": остановлен");
+            Dispatcher.Invoke(() => label.Content = "Читатель " + (id + 1) + ": остановлен");
         }
 
         private void WriterLoop(int id)
         {
             var label = writerLabels[id];
+            int? freeCell;
             while (running)
             {
-                Dispatcher.Invoke(() => label.Content = "Писатель " + id + 1 + ": ждёт");
-                QueueAdd("W-" + id + 1);
+                Dispatcher.Invoke(() => label.Content = "Писатель " + (id + 1) + ": ждёт");
+                QueueAdd("W" + id + 1);
 
-                mutex.Wait();
-                QueueRemove("W-" + id + 1);
+                freeCell = WriterFindFreeCell();
+                while (freeCell == null)
+                {
+                    Thread.Sleep(100);
+                    freeCell = WriterFindFreeCell();
+                }
 
-                Dispatcher.Invoke(() => label.Content = "Писатель " + id + 1 + ": пишет");
+                if (!cellLocks[(int)freeCell].TryAcquire())
+                {
+                    continue;
+                }
 
-                value = rand.Next(0, 1000);
-                Dispatcher.Invoke(() => SharedValueBox.Text = value.ToString());
-                Thread.Sleep(rand.Next(3000, 7000));
+                QueueRemove("W" + id + 1);
 
-                mutex.Release();
-                Dispatcher.Invoke(() => label.Content = "Писатель " + id + 1 + ": завершил");
+                Dispatcher.Invoke(() => label.Content = "Писатель " + (id + 1) + ": пишет");
 
-                Thread.Sleep(rand.Next(3000, 7000));
+
+                Thread.Sleep(rand.Next(2000, 3000));
+                value[(int)freeCell] = rand.Next(0, 1000);
+                cellLocks[(int)freeCell].Release();
+
+                Dispatcher.Invoke(() => label.Content = "Писатель " + (id + 1) + ": завершил");
+                UpdateArrayPanel();
+                Thread.Sleep(rand.Next(5000, 8000));
             }
-            Dispatcher.Invoke(() => label.Content = "Писатель " + id + 1 + ": остановлен");
+            Dispatcher.Invoke(() => label.Content = "Писатель " + (id + 1) + ": остановлен");
+        }
+
+        private int? ReaderFindFreeCell()
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] != null && !cellLocks[i].IsLocked)
+                {
+                    return i;
+                }
+            }
+
+            return null;
+        }
+
+        private int? WriterFindFreeCell()
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (value[i] == null && !cellLocks[i].IsLocked)
+                {
+                    return i;
+                }
+            }
+
+            return null;
         }
 
         private void QueueAdd(string name)
         {
             Dispatcher.Invoke(() =>
             {
-                if (QueueBox.Text == "-") QueueBox.Text = name;
-                else QueueBox.Text += " > " + name;
+                if (QueueBox.Text == "-")
+                {
+                    QueueBox.Text = name;
+                }
+                else
+                {
+                    QueueBox.Text += " - " + name;
+                }
             });
         }
 
@@ -163,10 +253,24 @@ namespace ReadersWriters
         {
             Dispatcher.Invoke(() =>
             {
-                var items = QueueBox.Text.Split('>', StringSplitOptions.TrimEntries);
-                QueueBox.Text = string.Join(" > ", items.Where(i => i != name));
-                if (string.IsNullOrWhiteSpace(QueueBox.Text))
-                    QueueBox.Text = "-";
+                lock (lockObj)
+                {
+                    var items = QueueBox.Text.Split('-', StringSplitOptions.TrimEntries);
+                    QueueBox.Text = string.Join(" - ", items.Where(i => i != name));
+                    if (string.IsNullOrWhiteSpace(QueueBox.Text))
+                    {
+                        QueueBox.Text = "-";
+                    }
+                }
+            });
+        }
+
+        private void UpdateArrayPanel()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (value == null) return;
+                ArrayPanel.ItemsSource = value.Select(v => v?.ToString() ?? "·").ToList();
             });
         }
     }
---
+++ ThreadSync/TrackedMutex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSync
+{
+    public class TrackedMutex
+    {
+        public Mutex Mutex { get; }
+        public bool IsLocked { get; private set; }
+
+        public TrackedMutex()
+        {
+            Mutex = new Mutex();
+            IsLocked = false;
+        }
+
+        public bool TryAcquire()
+        {
+            lock (this)
+            {
+                if (!IsLocked && Mutex.WaitOne(0))
+                {
+                    IsLocked = true;
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public void Release()
+        {
+            lock (this)
+            {
+                if (IsLocked)
+                {
+                    Mutex.ReleaseMutex();
+                    IsLocked = false;
+                }
+            }
+        }
+    }
+}
